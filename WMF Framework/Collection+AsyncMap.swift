797ba4f8be3745c313ef868ba705109885a95acc
joewalsh <joewalsh@users.noreply.github.com>
2018-10-29 09:45:46 -0400

    Revert "Add async map tests & fix out-of-order bugs"

--- WMF Framework/Collection+AsyncMap.swift
+++ WMF Framework/Collection+AsyncMap.swift
@@ -1,37 +1,37 @@
 import Foundation
 
-public extension Collection {
+extension Collection {
     func asyncMap<R>(_ block: (Element, @escaping (R) -> Void) -> Void, completion:  @escaping ([R]) -> Void) {
         let group = DispatchGroup()
-        var results = [R?](repeating: nil, count: count)
-        for (index, object) in self.enumerated() {
+        var results: [R] = []
+        for object in self {
             group.enter()
             block(object, { result in
-                results[index] = result
+                results.append(result)
                 group.leave()
             })
         }
         group.notify(queue: DispatchQueue.global(qos: .default)) {
-            completion(results as! [R])
+            completion(results)
         }
     }
 
     func asyncCompactMap<R>(_ block: (Element, @escaping (R?) -> Void) -> Void, completion:  @escaping ([R]) -> Void) {
         let group = DispatchGroup()
-        var results = [R?](repeating: nil, count: count)
-        for (index, object) in self.enumerated() {
+        var results: [R] = []
+        for object in self {
             group.enter()
             block(object, { result in
                 guard let result = result else {
                     group.leave()
                     return
                 }
-                results[index] = result
+                results.append(result)
                 group.leave()
             })
         }
         group.notify(queue: DispatchQueue.global(qos: .default)) {
-            completion(results.compactMap{$0})
+            completion(results)
         }
     }
     
